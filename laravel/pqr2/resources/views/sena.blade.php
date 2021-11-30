699df83a62b719d85df4773a5ed9cad84a38b1f8
Daniel Roman Caro <danielrc@gmail.com>
2021-11-30 18:54:29 -0500

    ACTIVIDAD DIA 30 DE NOVIEMBRE

---
+++ laravel/pqr2/resources/views/sena.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina SENA</title>
+    <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="card">
+        <div class="card-content">
+            <div class="alert alert-info">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi ea nulla harum cupiditate omnis perferendis quaerat ut nam odio nisi quo obcaecati at, accusamus dolor eum enim ratione sunt nostrum!</div>
+            <div class="alert alert-info">Accusamus assumenda odit autem impedit labore, deleniti quis quibusdam porro iusto voluptatem atque suscipit quidem amet laudantium qui voluptatibus enim voluptatum cupiditate eum voluptas minus saepe? Omnis incidunt recusandae sunt.</div>
+            <div class="alert alert-info">Tempore commodi dolor quae, facere saepe, culpa perferendis optio asperiores impedit error esse at earum ipsam quidem perspiciatis dolore fugit quo consequuntur architecto necessitatibus molestias voluptatibus reprehenderit, iusto illum! Placeat!</div>
+            <div class="alert alert-info">Esse, doloremque earum ducimus aliquam, reiciendis praesentium eius facere reprehenderit deserunt magnam est perspiciatis repellendus quos, sapiente maiores at quo. Quo, assumenda fugiat maxime nemo ullam nostrum? Nesciunt, odit ipsum.</div>
+            <div class="alert alert-info">Tenetur a architecto facilis maiores similique cupiditate earum deleniti. Aut quaerat sed voluptatum? Cupiditate doloremque nostrum eos expedita deleniti illum atque, sit fugiat iste dicta dolores nemo eaque, eius unde?</div>
+            <div class="col-12">guardar<button class="btn btn-primary"></button></div>
+        </div>
+    </div>
+    
+</body>
+</html>
